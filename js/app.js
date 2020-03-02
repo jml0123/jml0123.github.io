bac1ece4c40e65eafc0d5a532b9ea66fc297b542
Miguel Lorenzo <jsmglorenzo@gmail.com>
2020-03-02 01:06:02 -0500

    added links to work, landing page css anims

---
+++ js/app.js
@@ -0,0 +1,14 @@
+    
+    
+randomizeParticle = (target) => {
+        const width = target.style.width = `${2.5 + Math.random() * 8}vw`;
+        target.style.height = width;
+        target.style.left = `${5 + Math.random() * 95}%`;
+        target.style.animationDelay = `${Math.random() * 12 - 5}s`;
+        target.style.animationDuration = `${12 + Math.random() * 17}s`;
+}
+
+
+const particles = document.querySelectorAll(".particles li")
+particles.forEach(randomizeParticle);
+particles.addEventListener ("animationend", particles.forEach(randomizeParticle))

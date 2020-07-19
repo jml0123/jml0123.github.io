2c18683ea5af037dbaf6bd147d403674d0a1c9a9
Miguel Lorenzo <jsmglorenzo@gmail.com>
2020-07-18 23:37:11 -0400

    added new content and styles

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
     
     
 randomizeParticle = (target) => {
-        const width = target.style.width = `${2.5 + Math.random() * 8}vw`;
+        const width = target.style.width = `${6 + (Math.random() * 4)}vw`;
         target.style.height = width;
         target.style.left = `${5 + Math.random() * 95}%`;
         target.style.animationDelay = `${Math.random() * 12 - 5}s`;

f68f4d966e068266199901c32c131054708559a3
Miguel Lorenzo <jsmglorenzo@gmail.com>
2020-07-21 23:57:04 -0400

    changed icon sizes on landing page

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
     
     
 randomizeParticle = (target) => {
-        const width = target.style.width = `${6 + (Math.random() * 4)}vw`;
+        const width = target.style.width = `${32 + screen.width * 0.05}px`
         target.style.height = width;
         target.style.left = `${5 + Math.random() * 95}%`;
         target.style.animationDelay = `${Math.random() * 12 - 5}s`;

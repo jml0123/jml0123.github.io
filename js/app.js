b89d2c79c40bbea9775b05ca6d5011cb220e6f18
Miguel Lorenzo <jsmglorenzo@gmail.com>
2020-08-23 00:59:23 -0400

    fixed socials-row in landing page, rm unusued content

--- js/app.js
+++ js/app.js
@@ -7,29 +7,30 @@ randomizeParticle = (target) => {
 }
 
 document.addEventListener("DOMContentLoaded", function(){
-    const particles = document.querySelectorAll(".particles li")
-    const spotifyDiv = document.querySelector(".spotify-now-playing-widget")  
-    const spotifyIcon = `
-    <a href="https://open.spotify.com/user/jmlorenzo96?si=fgeLfn04R9qkn0YFqiS_0g"><img class="socials-icon" src="https://image.flaticon.com/icons/svg/2111/2111624.svg" alt="Spotify"></a>`
-    particles.forEach(randomizeParticle);
-    spotifyDiv.innerHTML = spotifyIcon
+    //const particles = document.querySelectorAll(".particles li")
+    //particles.forEach(randomizeParticle);
+    const socialsDiv = document.querySelector(".socials-wrapper")  
     fetch("https://serene-plains-09848.herokuapp.com/currently-playing",).then(res => res.json()).then(data => {
         const songData = !data.track? null :
-        `<a href=${data.url}>
+        `
+        <div class="spotify-now-playing-widget">
+        <a href=${data.url}>
                 <div class="badge">
                         <p class="badge-details">
                         <span role="img"aria-label="music-note">&nbsp🎵</span/><span id="now-playing">Now Playing: </span>
                         <span id="track-info">${data.artist} - ${data.track}</span>
                         </p>
                 </div>
-        </a>`
+        </a>
+        </div>
+        `
         if (songData){
                 console.log(`~~ NOW PLAYING: ${data.track} by ${data.artist} ~~`)
         }
         const template = `
         ${songData}
         `
-        spotifyDiv.innerHTML += template;
+        socialsDiv.innerHTML += template;
     }).catch(err => {
         console.log(err)
     })
